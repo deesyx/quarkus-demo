c7af284c1ab99ab94b7c5138c4e2a3288793a661
xuanguang.hu <xuanguang.hu@thoughtworks.com>
2020-12-04 16:54:27 +0800

    commit new client v2

--- src/main/java/org/acme/client/CountriesClient.kt
+++ src/main/java/org/acme/client/CountriesClient.kt
@@ -2,10 +2,7 @@ package org.acme.client
 
 import org.acme.external.Country
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient
-import javax.ws.rs.GET
-import javax.ws.rs.Path
-import javax.ws.rs.PathParam
-import javax.ws.rs.Produces
+import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
 
 @Path("/v2")
@@ -14,5 +11,6 @@ interface CountriesClient {
     @GET
     @Path("/name/{name}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     fun getByName(@PathParam("name") name: String): List<Country>
 }
