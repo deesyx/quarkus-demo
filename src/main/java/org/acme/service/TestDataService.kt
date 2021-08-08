4f56f3f60e29c9a9dfb0d76f5d8715f31d52f602
xuanguang.hu <xuanguang.hu@thoughtworks.com>
2021-08-08 16:19:02 +0800

    20210808-1

--- src/main/java/org/acme/service/TestDataService.kt
+++ src/main/java/org/acme/service/TestDataService.kt
@@ -4,11 +4,14 @@ import org.acme.domain.TestData
 import org.acme.entity.TestDataEntity
 import org.acme.repository.TestDataEntityRepository
 import org.eclipse.microprofile.context.ManagedExecutor
+import java.lang.IllegalArgumentException
 import java.util.concurrent.CompletableFuture
-import javax.enterprise.context.ApplicationScoped
 import javax.inject.Inject
+import javax.inject.Singleton
+import javax.transaction.Transactional
+import javax.transaction.UserTransaction
 
-@ApplicationScoped
+@Singleton
 class TestDataService {
 
     @Inject
@@ -17,14 +20,24 @@ class TestDataService {
     @Inject
     lateinit var managedExecutor: ManagedExecutor
 
+    @Inject
+    lateinit var transaction: UserTransaction
+
+    @Transactional
     fun test() {
+        println("thread: ${Thread.currentThread().name}, transaction: $transaction")
         val testData = TestData.create()
         testDataEntityRepository.save(TestDataEntity.ModelMapper.from(testData))
         val supplyAsync = CompletableFuture.supplyAsync({ update(testData) }, managedExecutor::execute)
+//        supplyAsync.join()
+//        throw IllegalArgumentException("test")
     }
 
     private fun update(testData: TestData) {
+        println("sub-thread-1: ${Thread.currentThread().name}, transaction: $transaction")
         testData.increaseUpdateCount()
+        Thread.sleep(5000)
+        println("sub-thread-2: ${Thread.currentThread().name}, transaction: $transaction")
         testDataEntityRepository.update(TestDataEntity.ModelMapper.from(testData))
     }
 }
